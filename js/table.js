ceab3bc8882b0db90437781b912fc6e447013c0c
irinazhuravleva <iravizion@gmail.com>
2023-04-14 21:09:34 +0100

    Title on Dictionary is adjusted

--- js/table.js
+++ js/table.js
@@ -16,11 +16,9 @@ function setTable(arr) {
 }
 
 function filterItems(e) {
-    // debugger
     var searchedText = e.target.value.toLowerCase();
     var items = [...document.querySelectorAll('tr.stroke')];
     items.forEach(function(item){
-        // var itemText = item.firstChild.textContent.toLowerCase();
         var itemText = item.innerText.split('\t').join(', ');
         if (itemText.indexOf(searchedText) != -1) {
             item.style.display= 'table-row';

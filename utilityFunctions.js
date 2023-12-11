862569924a6906c619cc628f8cc537055ea67345
Irina Zhuravleva <iravizion@gmail.com>
2023-12-11 16:15:32 +0000

    Test is changed

--- utilityFunctions.js
+++ utilityFunctions.js
@@ -1,5 +1,4 @@
-const isCyrillic = (str) => /^[а-яё]+$/i.test(str);
-const isLatin = (str) => /^[a-zA-Z]+$/.test(str);
-
+const isCyrillic = (str) => /[а-яё.,!?/]/i.test(str);
+const isLatin = (str) => /[a-zA-Z.,!?/]/.test(str);
 // Экспорт функций
 module.exports = { isCyrillic, isLatin };

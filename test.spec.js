65c287adc150d778f7e90da422614a076d01223e
Irina Zhuravleva <iravizion@gmail.com>
2024-02-03 13:09:05 +0000

    Tests are changed

--- test.spec.js
+++ test.spec.js
@@ -1,24 +1,29 @@
-const utilityFunctions = require('./utilityFunctions');
-const baseModule = require('./js/base.js');
+const objectBase = require('./js/base.js');
+const chalk = require('chalk');
 
-// Ваши тесты здесь
+const isCyrillic = (str) => /[а-яёA-Z.,!?/()\d]/.test(str);
+const isLatin = (str) => /[a-zA-Z.,!?/()\d]/.test(str);
 
-
-test('Check language for each element in the array', () => {
-    base.forEach((item) => {
+test('Check Cyrillic language for each element in the array', () => {
+    objectBase.base.forEach((item) => {
         expect(isCyrillic(item.question)).toBe(true);
+    });
+});
+
+test('Check Latin language for each element in the array', () => {
+    objectBase.base.forEach((item) => {
         expect(isLatin(item.answer)).toBe(true);
     });
 });
 
 test('Check for duplicates in questions', () => {
-    const array1 = base.map(item => item.question);
+    const array1 = objectBase.base.map(item => item.question);
     const array2 = [...new Set(array1)];
     expect(array1.every((value, index) => value === array2[index])).toBe(true);
 });
 
 test('Check for duplicates in answers', () => {
-    const array1 = base.map(item => item.answer);
+    const array1 = objectBase.base.map(item => item.answer);
     const array2 = [...new Set(array1)];
     expect(array1.every((value, index) => value === array2[index])).toBe(true);
 });
